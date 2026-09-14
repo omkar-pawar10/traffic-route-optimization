b617275364df30aa6cc1fe3a43ab2aec43d2dd9e
unknown <opomkar185@gmail.com>
2026-09-14 18:44:17 +0530

    style: aggressively align splash logo vertically

--- components/splash-screen.tsx
+++ components/splash-screen.tsx
@@ -45,7 +45,7 @@ export function SplashScreen() {
           src="/splash-icon.png"
           alt="MargDarshak Logo"
           fill
-          className="object-contain translate-x-[2%] -translate-y-[10%]"
+          className="object-contain translate-x-[3%] -translate-y-[20%]"
           priority
         />
       </div>
